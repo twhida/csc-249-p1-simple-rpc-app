7d191b30f25f4281d88c165630b21f496b8266be
Brant Cheikes <brant.cheikes@outlook.com>
2023-09-19 17:05:08 -0400

    added milestone trace output

--- echo-server.py
+++ echo-server.py
@@ -5,14 +5,19 @@ import socket
 HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
 PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
 
+print("server starting - listening for connections at IP", HOST, "and port", PORT)
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST, PORT))
     s.listen()
     conn, addr = s.accept()
     with conn:
-        print(f"Connected by {addr}")
+        print(f"Connected established with {addr}")
         while True:
             data = conn.recv(1024)
             if not data:
                 break
+            print(f"Received client message: '{data!r}' [{len(data)} bytes]")
+            print(f"echoing '{data!r}' back to client")
             conn.sendall(data)
+
+print("server is done!")
