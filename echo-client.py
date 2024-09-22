d4b403758e6aa5520142dc56b6909d08a9019746
TWHIDA <THIDA@SMITH.EDU>
2024-09-22 00:25:11 -0400

    final_edits

--- echo-client.py
+++ echo-client.py
@@ -5,20 +5,18 @@ import socket
 HOST = "127.0.0.1"  # The server's hostname or IP address
 PORT = 65432  # The port used by the server
 
-order_type = input("Would you like your ice cream as a scoop, milkshake, or chipwich?\n")
-flavor = ("Specify a flavor: strawberry, chocolate, vanilla\n")
-syrup = ("Would you like no_syrup, chocolate_syrup, or cherry_syrup?\n")
-
-MSG = f"{order_type}:{flavor}:{syrup}"
-
 print("Welcome to Tomoko's Really Pretty Cool (RPC) Ice Cream Stand!\n")
+
+order_type = input("Would you like your ice cream as a scoop, milkshake, or chipwich?\nSpecify a flavor: strawberry, chocolate, vanilla, and indicate whether you would like no_syrup, chocolate_syrup, or cherry_syrup.\nSeparate your order by commas!\n")
+MSG = f"{order_type}"
+
 print("Customer identified! - Connecting you to server", HOST, "and port...", PORT)
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
-    print(f"connection established! '{MSG}'")
+    print(f"connection established! 'You ordered: {MSG}'")
     s.sendall(bytes(MSG, 'utf-8'))
     print("Ice cream order sent, thank you for your patience!\n")
     data = s.recv(1024)
 
-print(f"Order up! Your order is ready: '{data!r}' You paid: [{len(data)} bytes] Thank you for your visit!\n")
+print(f"Order up! Your order is ready: '{data!r}' Current bytes: [{len(data)} bytes] Thank you for your visit!\n")
 print("Your order was fulfilled. Time for a Lactaid?\n")
--- echo-server.py
+++ echo-server.py
@@ -5,7 +5,51 @@ import socket
 HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
 PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
 
-print("Server starting - opening up shop at ", HOST, "with our cool entryway door with model number ", PORT)
+#function for understanding the order from input
+def understand(client_order):
+    order = client_order.split(",")
+    order_type = order[0].strip()
+
+    if order_type == "scoop":
+        flavor = order[1].strip()
+        if len(order) > 1:
+            syrup = order[2].strip()
+            if syrup == "no_syrup":
+                return f"Here is your {flavor} {order_type}!"
+            if syrup == "chocolate_syrup":
+                return f"Here is your {flavor} {order_type} with chocolate syrup!"
+            if syrup == "cherry_syrup":
+                return f"Here is your {flavor} {order_type} with cherry syrup!"
+        else:
+            return "Here is your scoop of mystery flavor... Please try ordering again!"
+    elif order_type == "milkshake":
+        flavor = order[1].strip()
+        if len(order) > 1:
+            syrup = order[2].strip()
+            if syrup == "no_syrup":
+                return f"Here is your {flavor} {order_type}!"
+            if syrup == "chocolate_syrup":
+                return f"Here is your {flavor} {order_type} with chocolate syrup!"
+            if syrup == "cherry_syrup":
+                return f"Here is your {flavor} {order_type} with cherry syrup!"
+        else:
+            return "Here is your milkshake with mystery flavor... Please try ordering again!"
+    elif order_type == "chipwich":
+        flavor = order[1].strip()
+        if len(order) > 1:
+            syrup = order[2].strip()
+            if syrup == "no_syrup":
+                return f"Here is your {flavor} {order_type}!"
+            if syrup == "chocolate_syrup":
+                return f"Here is your {flavor} {order_type} with chocolate syrup!"
+            if syrup == "cherry_syrup":
+                return f"Here is your {flavor} {order_type} with cherry syrup!"
+        else:
+            return "Here is your chipwich with mystery flavor... Please try ordering again!"
+    else:
+        return "You did not specify an order type! Please try ordering again."
+
+print("Server starting - opening up shop at", HOST, "with our cool entryway door with model number", PORT)
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST, PORT))
     s.listen()
@@ -13,14 +57,16 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     with conn:
         print("Successfully opened shop: We are open to orders!\n")
         while True:
-            #data should be parsed/evaluated
             data = conn.recv(1024)
             if not data:
                 break
             #this should be the decoded order
-            print(f"Received client order: '{data!r}' Payment due: [{len(data)} bytes]\n")
+            client_order = data.decode('utf-8')
+            print(f"Received client order: '{client_order}' Current bytes: [{len(data)} bytes]\n")
+
             #here it should also give back the decoded order
-            print(f"Serving '{data!r}' to the to customer!\n")
-            conn.sendall(data)
+            order_fulfillment = understand(client_order)
+            print(f"Saying'{order_fulfillment}' to the to customer!\n")
+            conn.sendall(order_fulfillment.encode('utf-8'))
 
 print("Really Pretty Cool ice cream has been served, order complete! Closing shop...\n")
