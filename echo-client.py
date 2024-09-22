fd0c997931c780b754f162ccb69fad545804d80b
TWHIDA <THIDA@SMITH.EDU>
2024-09-21 22:33:02 -0400

    prelim_message_edits

--- echo-client.py
+++ echo-client.py
@@ -4,15 +4,15 @@ import socket
 
 HOST = "127.0.0.1"  # The server's hostname or IP address
 PORT = 65432  # The port used by the server
-MSG = "Hello, world"
+MSG = ("Welcome to Tomoko's Really Pretty Cool (RPC) Ice Cream Stand!\nFlavors: strawberry, chocolate, vanilla\nWe offer: scoop, milkshake, chipwich\nToppings: no_topping, whipped_cream, sprinkles\nPlease indicate your order by writing the flavor, ice cream type, and toppings (or lack thereof) in order.")
 
-print("client starting - connecting to server at IP", HOST, "and port", PORT)
+print("Customer identified! - Connecting you to server", HOST, "and port...", PORT)
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
-    print(f"connection established, sending message '{MSG}'")
+    print(f"connection established! sending your unique Really Pretty Cool order... '{MSG}'")
     s.sendall(bytes(MSG, 'utf-8'))
-    print("message sent, waiting for reply")
+    print("Ice cream order sent, thank you for your patience!")
     data = s.recv(1024)
 
-print(f"Received response: '{data!r}' [{len(data)} bytes]")
-print("client is done!")
+print(f"Order up! Your order is ready: '{data!r}' You paid: [{len(data)} bytes] Thank you for your visit!")
+print("Your oder was fulfilled. Time for a Lactaid?")
--- echo-server.py
+++ echo-server.py
@@ -5,7 +5,7 @@ import socket
 HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
 PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
 
-print("server starting - listening for connections at IP", HOST, "and port", PORT)
+print("Server starting - opening up shop at ", HOST, "with our cool entryway door with model number ", PORT)
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST, PORT))
     s.listen()
@@ -13,11 +13,14 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     with conn:
         print(f"Connected established with {addr}")
         while True:
+            #data should be parsed/evaluated
             data = conn.recv(1024)
             if not data:
                 break
-            print(f"Received client message: '{data!r}' [{len(data)} bytes]")
-            print(f"echoing '{data!r}' back to client")
+            #this should be the decoded order
+            print(f"Received client order: '{data!r}' Payment due: [{len(data)} bytes]")
+            #here it should also give back the decoded order
+            print(f"serving '{data!r}' to the to customer!")
             conn.sendall(data)
 
-print("server is done!")
+print("Really Pretty Cool ice cream has been served, order complete!")
